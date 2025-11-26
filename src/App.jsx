0244dff87f401f8e5dff715d8d9713d9fde6cf12
Julian Mendoza <mendoza.julian2278@gmail.com>
2025-11-25 16:17:51 -0800

    Changed background

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <img src={"https://th.bing.com/th/id/R.d2b5043acd2ed124c72cdaddd6a9db1d?rik=oQt8B%2bdkLpHp1w&pid=ImgRaw&r=0"} />
-      <h1>Welcome to Naitry!</h1>
+      <h1>Welcome to Dr. Pepper!</h1>
     </>
   )
 }
