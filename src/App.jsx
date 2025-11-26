5f08e94f7b02129a6ee7e605ba24b9e4c45f6e48
Julian Mendoza <mendoza.julian2278@gmail.com>
2025-11-25 16:12:48 -0800

    Rebranding to Dr. Pepper

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ function App() {
 
   return (
     <>
-      <p>Hello there</p>
+      <img src={"https://th.bing.com/th/id/R.d2b5043acd2ed124c72cdaddd6a9db1d?rik=oQt8B%2bdkLpHp1w&pid=ImgRaw&r=0"} />
+      <h1>Welcome to Naitry!</h1>
     </>
   )
 }
